81ba8fc84083be01dc85a47839fab2008280fd50
salmataherfouad71-dev <salma.taherfouad71@gmail.com>
2026-08-05 20:41:49 +0300

    Shipment

--- Assignment.6/DeliveryAddress.cs
+++ Assignment.6/DeliveryAddress.cs
@@ -21,5 +21,9 @@ namespace Assignment._6
         {
             return $"City:{city}\nStreet:{street}\nBuilding Number:{buildingNumber}";
         }
+        public override string ToString()
+        {
+            return $"City:{city}\nStreet:{street}\nBuilding Number:{buildingNumber}";
+        }
     }
 }
---
+++ Assignment.6/Shipment.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment._6
+{
+    internal struct Shipment
+    {
+        private string trackingCode;
+        private string description;
+        private double weight;
+        private decimal deliveryFee;
+        public DeliveryAddress destination { set; get; }
+        public string TrackingCode
+        {
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    trackingCode = value;
+            }
+            get { return trackingCode; }
+        }
+        public string Description
+        {
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    description = value;
+            }
+            get { return description; }
+        }
+        public double Weight
+        {
+            set
+            {
+                if (value > 0)
+                    weight = value;
+            }
+            get { return weight; }
+        }
+        public decimal DeliveryFee
+        {
+            private set
+            {
+                if (value > 0)
+                    deliveryFee = value;
+            }
+            get { return deliveryFee; }
+        }
+        public decimal EstimatedCost
+        {
+            get { return deliveryFee+((decimal)weight *5); }
+        }
+        public Shipment(string trackingCode)
+        {
+            this.trackingCode = trackingCode;
+            description = "Unknown";
+            weight = 1;
+            deliveryFee = 50;
+            destination = default;
+        }
+        public Shipment(string trackingCode, string description, double weight, decimal deliveryFee, DeliveryAddress destination)
+        {
+            TrackingCode=trackingCode;
+            Weight = weight;
+            Description = description;
+            DeliveryFee= deliveryFee;
+            this.destination=destination;
+        }
+        public void UpDateDiliveryFee(decimal newFee)
+        {
+            if (newFee > 0)
+                deliveryFee= newFee;
+        }
+        public void PrintShipment()
+        {
+            Console.WriteLine($"Tracking Code:{trackingCode}\nDescription:{description}");
+            Console.WriteLine($"Weight:{weight}\nDelivery Fee:{deliveryFee}");
+            Console.WriteLine($"Destination:{destination}\nEstimatedCost:{EstimatedCost}");
+        }
+    }
+}
