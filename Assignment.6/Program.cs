5171b4b8195e03f65926916702ab7b3a4f0c6032
salmataherfouad71-dev <salma.taherfouad71@gmail.com>
2026-08-05 11:03:39 +0300

    Question01

--- Assignment.6/Program.cs
+++ Assignment.6/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            #region Part01
+            #region Question01
+            /*a) The original value will not change because the struct is value type.
+              b) The other value also will not change although the class is reference type 
+                 because the string is immutable (when changing it ,it cerate another object).*/
+            #endregion
+            #endregion
         }
     }
 }
