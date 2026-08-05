b87e79d477217652d858c9d93d8d5ecd7d6d4153
salmataherfouad71-dev <salma.taherfouad71@gmail.com>
2026-08-05 14:50:32 +0300

    Question02

--- Assignment.6/Program.cs
+++ Assignment.6/Program.cs
@@ -10,6 +10,14 @@
               b) The other value also will not change although the class is reference type 
                  because the string is immutable (when changing it ,it cerate another object).*/
             #endregion
+            #region Question02
+            /*a) -Anyone can assign these fields invalid or unexpected values directly.
+                 -Cannot apply validation on the input.
+                 -Any Changes on the original field must also be applied on the objects in Main
+                  which makes it hard to maintenance.
+              b) Private fields prevent anyone to access the fields and change it's value
+                 and public property allow the validation on the input and set and get the values.*/
+            #endregion
             #endregion
         }
     }
